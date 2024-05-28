9acebdd2c5d7f9e72dd8e91aaaec99f1a0800fee
Atroci <hugomoreiradecarvalho@gmail.com>
2024-05-28 10:28:58 +0100

    update apointoo

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-module.exports = {
-  trailingSlash: true,
-  modularizeImports: {
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
-    },
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
-    },
-    '@mui/lab': {
-      transform: '@mui/lab/{{member}}',
-    },
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
