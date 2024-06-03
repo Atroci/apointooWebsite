c54ba3aca66908eb437a55093797f1e21c59951d
Atroci <hugomoreiradecarvalho@gmail.com>
2024-06-03 22:02:32 +0100

    new theme

---
+++ libs/menus.tsx
@@ -0,0 +1,146 @@
+export const menus = [
+  {
+    label: "Home",
+    link: "#",
+    submenu: [
+      {
+        label: "Home Demo 1",
+        link: "/",
+      },
+      {
+        label: "Home Demo 2",
+        link: "/home-2/",
+      },
+      {
+        label: "Home Demo 3",
+        link: "/home-3/",
+      },
+      {
+        label: "Home Demo 4",
+        link: "/home-4/",
+      },
+      {
+        label: "Home Demo 5",
+        link: "/home-5/",
+      },
+    ],
+  },
+  {
+    label: "About",
+    link: "#",
+    submenu: [
+      {
+        label: "About Style One",
+        link: "/about/",
+      },
+      {
+        label: "About Style Two",
+        link: "/about-2/",
+      },
+    ],
+  },
+  {
+    label: "Services",
+    link: "#",
+    submenu: [
+      {
+        label: "Services Style One",
+        link: "/services/",
+      },
+      {
+        label: "Services Style Two",
+        link: "/services/style-2/",
+      },
+      {
+        label: "Services Style Three",
+        link: "/services/style-3/",
+      },
+      {
+        label: "Services Style Four",
+        link: "/services/style-4/",
+      },
+      {
+        label: "Service Details",
+        link: "/services/service-details/",
+      },
+    ],
+  },
+  {
+    label: "Pages",
+    link: "#",
+    submenu: [
+      {
+        label: "Pricing",
+        link: "/pricing/",
+      },
+      {
+        label: "Team",
+        link: "/team/",
+      },
+      {
+        label: "Testimonials",
+        link: "/testimonials/",
+      },
+      {
+        label: "FAQ",
+        link: "/faq/",
+      },
+      {
+        label: "Login",
+        link: "/auth/login/",
+      },
+      {
+        label: "Sign Up",
+        link: "/auth/sign-up/",
+      },
+      {
+        label: "Terms & Conditions",
+        link: "/terms-conditions/",
+      },
+      {
+        label: "Privacy Policy",
+        link: "/privacy-policy/",
+      },
+      {
+        label: "Coming Soon",
+        link: "/coming-soon/",
+      },
+      {
+        label: "404 Error Page",
+        link: "/404/",
+      },
+    ],
+  },
+  {
+    label: "News",
+    link: "#",
+    submenu: [
+      {
+        label: "News Grid",
+        link: "/news/",
+      },
+      {
+        label: "News Right Sidebar",
+        link: "/news/news-right-sidebar/",
+      },
+      {
+        label: "News Details",
+        link: "/news/news-details/",
+      },
+    ],
+  },
+  {
+    label: "Contact",
+    link: "#",
+    submenu: [
+      {
+        label: "Contact Style One",
+        link: "/contact/",
+      },
+      {
+        label: "Contact Style Two",
+        link: "/contact-2/",
+      },
+    ],
+  },
+];
